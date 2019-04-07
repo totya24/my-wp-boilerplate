7e90b0620e706d9370c93a47339c23eb5e0cf44a
Tóth Zoltán <hello@totya.eu>
2019-04-07 19:19:31 +0200

    fix twig ajax issue

--- core/wptwig/wptwig.php
+++ core/wptwig/wptwig.php
@@ -12,10 +12,12 @@ class WpTwig
 
 	private function __construct()
 	{
-		add_action('init', array($this, 'setupTwigEnvironment'), 0, 0);
-		add_action('template_redirect', array($this, 'setGlobalVariables'), 9999);
+		if(!is_admin() || defined( 'DOING_AJAX' ) && DOING_AJAX){
+			add_action('init', array($this, 'setupTwigEnvironment'), 0, 0);
+			add_action('template_redirect', array($this, 'setGlobalVariables'), 9999);
+		}
 	}
-
+	
 	public static function getInstance()
 	{
 		if (null === self::$instance) {
