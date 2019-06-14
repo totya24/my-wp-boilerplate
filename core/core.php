88d97ecd2f468cc2a7ef555f68e77a0236b71c53
Tóth Zoltán <hello@totya.eu>
2019-06-14 21:10:38 +0200

    remove svg-related tweaks

--- core/core.php
+++ core/core.php
@@ -5,7 +5,6 @@ require_once('vendor/autoload.php');
 require_once('singleton.php');
 require_once('greedo.php');
 require_once('wptwig.php');
-require_once('svgpreview.php');
 require_once('optimize.php');
 require_once('archivelinks.php');
 require_once('customlistcolumns.php');
