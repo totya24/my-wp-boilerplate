161dc83b19f2beee67b8f3995bf6f723b1f244ac
Tóth Zoltán <hello@totya.eu>
2019-11-20 07:45:38 +0100

    remove title theme support avoid duplicate title tag

--- core/optimize.php
+++ core/optimize.php
@@ -14,7 +14,6 @@ Class Optimize
         add_filter( 'the_generator', '__return_empty_string' );
 
         # Theme supports - https://www.daddydesign.com/wordpress/how-to-add-features-in-wordpress-using-add_theme_support-function/
-        add_theme_support( 'title-tag' );
         add_theme_support( 'post-thumbnails' );
         add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
 
